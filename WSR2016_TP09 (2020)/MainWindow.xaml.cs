8a31e543c56e96c53339edd702d0ed02b7fdf799
rrm1 <rustamramazanovworkmail@gmail.com>
2020-02-29 15:16:30 +0300

    Добавлено: стартовая страница.

--- WSR2016_TP09 (2020)/MainWindow.xaml.cs
+++ WSR2016_TP09 (2020)/MainWindow.xaml.cs
@@ -1,28 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WSR2016_TP09__2020_
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            OpenPage(pages.HomePage);
+        }
+
+        // Список доступных страниц.
+        public enum pages
+        {
+            HomePage
+        }
+        
+        // Метод открытия страниц.
+        public void OpenPage(pages pages)
+        {
+            // Стартовая страница.
+            if (pages == pages.HomePage)
+                frame.Navigate(new HomePage(this));
         }
     }
 }
